81cb462fe964503a1b9bff47d5fcc87d935f772a
noitu <dimalego35@gmail.com>
2023-02-07 14:10:29 +0300

    create funcs for more tasks

--- MainPage.py
+++ MainPage.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow
 
@@ -10,21 +11,57 @@ class MainPage(QMainWindow, Ui_MainWindow):
         super().__init__()
 
         self.setupUi(self)
+        self.v_shift = 0  # Сдвиг карты по вертикали
+        self.h_shift = 0  # Сдвиг карты по горизонтали
+
+        self.zoom = 1  # Показатель зума
+
+        self.layer = 'map'  # текущий слой
+
+        self.check_click_for_index = False  # показатель из 9 задачи
+        self.point = None  # показатель из 6 задачи
 
         self.mapParser = MapParser()
 
         self.show_map()
 
-    def update_map(self):
-        pass
+    def check_for_index(self):
+        self.check_click_for_index = not self.check_click_for_index
+        self.mapParser.search_place()
 
-    def get_map(self):
-        pass
+    def set_layer(self):
+        if self.layer_map_btn.isChecked():
+            self.layer = 'map'
+        elif self.layer_sat_btn.isChecked():
+            self.layer = 'sat'
+        else:
+            self.layer = 'sat,skl'
 
     def show_map(self):
-        #loading map image
+        # loading map image
         map = QPixmap()
 
         map.loadFromData(self.mapParser.get_map_image())
 
         self.maps_view.setPixmap(map)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.zoom / 1.5 > 0.001:
+                self.zoom /= 1.5
+        elif event.key() == Qt.Key_PageDown:
+            if self.zoom * 1.5 < 90:
+                self.zoom *= 1.5
+        if event.key() == Qt.Key_Left:
+            if 0 < self.h_shift - self.zoom < 180:
+                self.h_shift -= self.zoom
+        if event.key() == Qt.Key_Up:
+            if -90 < self.v_shift + self.zoom < 90:
+                self.v_shift += self.zoom
+        if event.key() == Qt.Key_Right:
+            if 0 < self.h_shift + self.zoom < 180:
+                self.h_shift += self.zoom
+        if event.key() == Qt.Key_Down:
+            if -90 < self.v_shift - self.zoom < 90:
+                self.v_shift -= self.zoom
+        print(f'zoom: {self.zoom},h_shift: {self.h_shift},v_shift {self.v_shift}')
--- lib/maps_stuff/map_parser.py
+++ lib/maps_stuff/map_parser.py
@@ -4,7 +4,14 @@ import requests
 class MapParser:
     def __init__(self):
         self.url = "http://static-maps.yandex.ru/1.x/"
+        self.ll = None
+        self.spn = None
 
     def get_map_image(self):
-        # func must return bytes of the image
-        return "some_bytes"
+        s = requests.Session()
+        r = s.request('GET', self.url)
+        content = r.content  # bytes
+        return content
+
+    def search_place(self):
+        pass
