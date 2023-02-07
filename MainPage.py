2ca19b75f1d51541a78ea3762ebb4f2cdf2653f6
noitu <dimalego35@gmail.com>
2023-02-07 10:53:50 +0300

    create base structure

--- MainPage.py
+++ MainPage.py
@@ -1,6 +1,25 @@
-from ui_files.main_window import Ui_MainWindow
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow
 
-class MainPage(Ui_MainWindow):
+from lib.maps_stuff.map_parser import MapParser
+from lib.windows.main_window import Ui_MainWindow
+
+
+class MainPage(QMainWindow, Ui_MainWindow):
     def __init__(self):
+        super().__init__(self)
+
         self.setupUi(self)
 
+        self.mapParser = MapParser()
+
+    def update_map(self):
+        pass
+
+    def get_map(self):
+        pass
+
+    def show_map(self):
+        map = QPixmap(self.mapParser.get_map_image())
+
+        self.maps_view.setPixmap(map)
---
+++ lib/maps_stuff/map_parser.py
@@ -0,0 +1,7 @@
+class MapParser:
+    def __init__(self):
+        self.url = "http://static-maps.yandex.ru/1.x/"
+
+    def get_map_image(self):
+        # returns image object
+        pass
